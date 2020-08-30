f6d0bec3c4b67a7770e594fbcb2fe4093a80dfd1
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-30 13:50:55 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,20 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def checked
+    # クリックしたメモの情報取得
+    post = Post.find(params[:id])
+    # メモにチェックが入っていたら、チェックを外す
+    # メモにチェックが入っていなかったら、チェックをつける
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    # 更新したデータをitemに代入し、ビューに受けわたす
+    # javascript使用のため、JSON形式で渡す
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+
 end
